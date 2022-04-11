6e0a7f23946b2a68d08092924c857100ae5ec432
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-11 20:58:15 -0300

    teste

--- Exemplos/App.tsx
+++ Exemplos/App.tsx
@@ -1,15 +1,35 @@
 import React from "react";
-import { StatusBar ,Text , View } from "react-native";
+import { StatusBar ,Text , View, TextInput } from "react-native";
 
 
 export default class App extends React.Component{
+  
+  
+  constructor(props:any){
+    super(props)
+    this.state={
+      nome:'leandro'
+    }
+  }
+
   render(){
     return(
-      <View>
-        <Text>Olá mundo</Text>
-        <StatusBar/>
-      </View>
+      <>
+        <View style={{padding:16}}>
+            <TextInput
+              autoFocus
+              placeholder="informe seu nome..."
+              style={{
+                borderColor:'black',
+                borderWidth:1,
+                fontSize:30,
+                paddingHorizontal:8
+              }}
+            />
+            <Text>{this.state.nome}</Text>
+          <StatusBar/>
+        </View>
+      </>
     )
   }
 }
-
