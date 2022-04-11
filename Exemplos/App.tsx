ba59282ef898b6df5f96335d5575467973708bc9
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-11 19:56:07 -0300

    aula 1

--- Exemplos/App.tsx
+++ Exemplos/App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar ,Text , View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>Olá mundo</Text>
+        <StatusBar/>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
