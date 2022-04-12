2fb83ba5dee2626eeaebc544da307ebafeb8777e
leandroyyy <leandro.cavallari123@gmail.com>
2022-04-11 22:43:08 -0300

    aula finalizada

--- Exemplos/App.tsx
+++ Exemplos/App.tsx
@@ -1,39 +1,52 @@
 import React from "react";
-import { StatusBar ,Text , View, TextInput } from "react-native";
+import {StyleSheet, StatusBar, Text, TextInput, View} from "react-native";
 
-interface AppProps{
-  nome:string
+interface AppProps {
+  nome: string,
+  idade?: number,
 }
 
-export default class App extends React.Component{
-  
-  state:AppProps
-
-  constructor(props:any){
+export default class App extends React.Component {
+  state: AppProps
+  constructor(props: any) {
     super(props)
-    this.state={
-      nome:'teste'
+    this.state = {
+      nome: '',
+      idade: 12,
     }
   }
 
-  render(){
-    return(
+  mudouMensagem = (msg:string) => {
+    this.setState({nome:msg})
+  }
+  render() {
+    return (
       <>
-        <View style={{padding:16}}>
-            <TextInput
-              keyboardType='numeric'
-              placeholder="informe seu nome..."
-              style={{
-                borderColor:'black',
-                borderWidth:1,
-                fontSize:30,
-                paddingHorizontal:8
-              }}
-            />
-            <Text>{this.state.nome}</Text>
-          <StatusBar/>
+        <View style={styles.container}>
+          <TextInput
+          onChangeText={this.mudouMensagem} 
+          style={styles.input}></TextInput>
+          <Text style={styles.text}>{this.state.nome}</Text>
+          <StatusBar />
         </View>
       </>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding:16
+  },
+  input:{
+    borderColor:'gray',
+    borderWidth:0.5,
+    fontSize:28,
+    paddingHorizontal:5,
+    marginBottom:40
+  },
+  text:{
+    textAlign:'center',
+    fontSize:40
+  }
+})
